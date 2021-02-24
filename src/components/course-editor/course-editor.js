0e1f74b5181cbbfbd36f62cd4ce2282b3eeaf55b
firebearrex <rex.cheng@qq.com>
2021-02-25 00:41:54 +0800

    Adjusted layout

--- src/components/course-editor/course-editor.js
+++ src/components/course-editor/course-editor.js
@@ -6,14 +6,14 @@ const CourseEditor = ({history}) =>
     <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
             <div className="container-fluid row">
-                <span className={"col-1 col-lg-3"}>
+                <span className={"col-1 col-md-3 mr-auto"}>
                     <a onClick={() => history.goBack()}
                        className="navbar-brand fas fa-times"></a>
-                    <span className={"navbar-text h5 m-auto d-none d-lg-inline"}>
+                    <span className={"navbar-text h4 m-auto d-none d-md-inline text-white"}>
                         Course Editor
                     </span>
                 </span>
-                <span className={"col-11 col-lg-9"}>
+                <form className={"navbar-nav col-11 col-md-9"}>
                     <ul className="nav nav-pills nav-fill">
                         <li className="nav-item">
                             <a className="nav-link active" aria-current="page" href="#">
@@ -41,7 +41,7 @@ const CourseEditor = ({history}) =>
                             </a>
                         </li>
                     </ul>
-                </span>
+                </form>
             </div>
         </nav>
         {/*<div className={"extra-top-padding-editor"}>*/}
@@ -53,6 +53,62 @@ const CourseEditor = ({history}) =>
         {/*        Course Editor*/}
         {/*    </h2>*/}
         {/*</div>*/}
+
+        <div className="row extra-top-padding">
+            <div className="col-4 editor-module-list-background">
+                <div className="list-group list-group-flush mt-3 editor-module-list">
+                    <a href="#" className="list-group-item list-group-item-action active" aria-current="true">
+                        Cras justo odio
+                        <i className="fas fa-minus-circle float-right"></i>
+                    </a>
+                    <a href="#" className="list-group-item list-group-item-action">
+                        Dapibus ac facilisis in
+                        <i className="fas fa-minus-circle float-right"></i>
+                    </a>
+                    <a href="#" className="list-group-item list-group-item-action">
+                        Morbi leo risus
+                        <i className="fas fa-minus-circle float-right"></i>
+                    </a>
+                    <a href="#" className="list-group-item list-group-item-action">
+                        Porta ac consectetur ac
+                        <i className="fas fa-minus-circle float-right"></i>
+                    </a>
+                    <a href="#" className="list-group-item list-group-item-action">
+                        Vestibulum at eros
+                        <i className="fas fa-minus-circle float-right"></i>
+                    </a>
+                    <a href="#" className="list-group-item list-group-item-action">
+                        <i className="fas fa-plus-circle float-right"></i>
+                    </a>
+                </div>
+            </div>
+
+            <div className="col-8">
+                <ul className="nav nav-pills mt-3">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">Active</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">
+                            <i className="fas fa-plus-circle"></i>
+                        </a>
+                    </li>
+                </ul>
+
+                <br/>
+
+                Content intentionally left blank
+            </div>
+        </div>
     </div>
 
 export default CourseEditor;
