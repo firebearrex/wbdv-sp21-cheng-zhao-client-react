5cc69601aceb8cfffda7b29fe5ccba822035f508
firebearrex <60494405+firebearrex@users.noreply.github.com>
2021-03-22 12:06:17 +0800

    Fixed display issue with the heading

--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,41 @@
 import React from 'react'
 
+const ewq = ['qu','wf','eh','iu','tb'];
+
+const ewq2 = [28,107,26,65,95];
+
 function Hello() {
-  return <h1>Hello World!!!</h1>
+  return (
+      <ul>
+        {
+          ewq.map(rew => {
+                return <li>{rew}</li>
+              }
+          )
+        }
+      </ul>
+  )
 }
+
+function Test() {
+  return (
+      <ul>
+        {
+          ewq.filter(
+              function(dsa){
+                return dsa > 50
+              })
+              .map(
+                  function(rew) {
+                    return(
+                        <li>{rew}</li>
+                    )
+                  })
+        }
+      </ul>
+  )
+}
+
+
 
 export default Hello
--- src/components/widgets/heading-widget.js
+++ src/components/widgets/heading-widget.js
@@ -23,12 +23,12 @@ const HeadingWidget = (
                        className={"fas fa-2x fa-cog float-right"}></i>
                     <Link
                         to={to}>
-                        {widget.size === 1 && <h1>{widget.text}</h1>}
-                        {widget.size === 2 && <h2>{widget.text}</h2>}
-                        {widget.size === 3 && <h3>{widget.text}</h3>}
-                        {widget.size === 4 && <h4>{widget.text}</h4>}
-                        {widget.size === 5 && <h5>{widget.text}</h5>}
-                        {widget.size === 6 && <h6>{widget.text}</h6>}
+                        {(widget.size === 1 || widget.size === "1") && <h1>{widget.text}</h1>}
+                        {(widget.size === 2 || widget.size === "2") && <h2>{widget.text}</h2>}
+                        {(widget.size === 3 || widget.size === "3") && <h3>{widget.text}</h3>}
+                        {(widget.size === 4 || widget.size === "4") && <h4>{widget.text}</h4>}
+                        {(widget.size === 5 || widget.size === "5") && <h5>{widget.text}</h5>}
+                        {(widget.size === 6 || widget.size === "6") && <h6>{widget.text}</h6>}
                     </Link>
                 </>
             }
